11de2ad87001396f6ad0a73d89477b48c56cae8c
Gabriel Moscarde <78855746+Moscarde@users.noreply.github.com>
2022-07-04 03:49:38 -0300

    Create div-class-preview.js

---
+++ div-class-preview.js
@@ -0,0 +1,74 @@
+
+const styleP = `
+position: absolute;
+right: 0;
+bottom: 0;
+font-size: 12px;
+font-style: italic;
+color: red;
+text-shadow: 0 0 6px #000;`
+
+const styleMenu = `
+position: absolute;
+top: 30px;
+right: 30px;
+background: red;
+padding: 30px;`
+
+const showInfo = elementList => {
+    for (let i = 0; i <= elementList.length; i++) {
+        if (elementList[i] == undefined) {
+            continue
+        }
+        elementList[i].innerHTML += `
+        <p style="${styleP}" >
+        ${elementList[i].className}
+        </p>`
+
+        elementList[i].classList.add('display-info')
+        elementList[i].style.border = "solid 1px red"
+        if (elementList[i].style.position == "") {
+            elementList[i].style.position = "relative";
+        }
+    }
+}
+
+
+const hideInfo = elementList => {
+    console.log('entr');
+    for (let i = 0; i <= elementList.length; i++) {
+        if (elementList[i] == undefined) {
+            continue
+        }
+        elementList[i].classList.remove('display-info')
+    }
+    let nameInfo = document.querySelectorAll('.info-nome')
+    nameInfo.forEach(p => {
+        p.style.display = "none"
+    })
+}
+
+document.querySelector("header").innerHTML += `
+<div style="${styleMenu}">
+Display Id & Class Name Menu
+</br>
+<button id="displayOn">Ligar</button>
+</div>`
+
+
+
+document.getElementById("displayOn").onclick = function () {
+    let divList = document.getElementsByTagName("div")
+    let sectionList = document.getElementsByTagName("section")
+    showInfo(divList)
+    showInfo(sectionList)
+}
+
+// document.getElementById("displayOff").onclick = function () {
+//     console.log('btn');
+//     let divList = document.getElementsByTagName("div")
+//     let sectionList = document.getElementsByTagName("section")
+//     hideInfo(divList)
+//     hideInfo(sectionList)
+
+// }
